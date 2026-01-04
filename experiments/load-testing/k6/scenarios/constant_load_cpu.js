2cdac53e181011666baf057953d3fac43e5f025a
Jakub Kokoszka <jakub.kokoszka@softwaremind.com>
2026-01-04 20:24:10 +0100

    Scenarios update

--- experiments/load-testing/k6/scenarios/constant_load_cpu.js
+++ experiments/load-testing/k6/scenarios/constant_load_cpu.js
@@ -2,24 +2,18 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 const BASE_URL = __ENV.BASE_URL || 'https://workload-app.mgr.kokoszka.cloud';
-const VUS = __ENV.VUS || 40;
-const DURATION = __ENV.DURATION || '30s';
+const VUS = __ENV.VUS || 100;
+const DURATION = __ENV.DURATION || '120s';
 
 export let options = {
     vus: parseInt(VUS),
     duration: DURATION,
+    httpReqTimeout: '30s',
 };
 
 export default function () {
     let cpuRes = http.get(`${BASE_URL}/cpu?loops=1000`);
     check(cpuRes, { 'cpu 200': (r) => r.status === 200 });
 
-    let memRes = http.get(`${BASE_URL}/mem?mb=10`);
-    check(memRes, { 'mem 200': (r) => r.status === 200 });
-
-    let ioRes = http.get(`${BASE_URL}/io?size=1`);
-    check(ioRes, { 'io 200': (r) => r.status === 200 });
-
-    sleep(1);
 }
 
--- experiments/load-testing/k6/scenarios/ramp_up.js
+++ experiments/load-testing/k6/scenarios/ramp_up.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const BASE_URL = __ENV.BASE_URL || 'http://localhost:8000';
+const BASE_URL = __ENV.BASE_URL || 'http://workload-app.mgr.kokoszka.cloud';
 
 export let options = {
     stages: [
